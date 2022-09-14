cbc234b89335cc374cf6c0b06f7a6e534c1c0db3
frijolito <jeffgray@virginia.edu>
2022-09-13 20:16:06 -0400

    get current room exits idea, testing webhook

--- room.rb
+++ room.rb
@@ -10,8 +10,21 @@ class Room
     @s = s
     @w = w
     @objects = objects
+    @exits = get_current_room_exits # TODO: make sure this works
   end
 
+  # This is shitty code but we will probably redo rooms at some point
+  # making this method instead of having [exits] in initializer since we build exits in world_map
+  # We might keep this or just use adjacent rooms
+  # TODO: Uncomment this or delete 
+  # def get_current_room_exits
+  #   exits = []
+  #   exits.append("North") if @n != -1
+  #   exits.append("East") if @e != -1
+  #   exits.append("South") if @s != -1
+  #   exits.append("West") if @w != -1
+  #   exits 
+  # end
 
   # def scan_difficulty_table
   #   {
