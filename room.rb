b1895a8100a5ec07966fad29c5fce0e46b4afdb8
frijolito <jeffgray@virginia.edu>
2022-09-13 20:21:56 -0400

    QOL update showing exits at each move

--- room.rb
+++ room.rb
@@ -13,18 +13,17 @@ class Room
     @exits = get_current_room_exits # TODO: make sure this works
   end
 
-  # This is shitty code but we will probably redo rooms at some point
   # making this method instead of having [exits] in initializer since we build exits in world_map
-  # We might keep this or just use adjacent rooms
   # TODO: Uncomment this or delete 
-  # def get_current_room_exits
-  #   exits = []
-  #   exits.append("North") if @n != -1
-  #   exits.append("East") if @e != -1
-  #   exits.append("South") if @s != -1
-  #   exits.append("West") if @w != -1
-  #   exits 
-  # end
+    # We might keep this or just use adjacent rooms
+  def get_current_room_exits
+    exits = []
+    exits.append("North") if @n != -1
+    exits.append("East") if @e != -1
+    exits.append("South") if @s != -1
+    exits.append("West") if @w != -1
+    exits 
+  end
 
   # def scan_difficulty_table
   #   {
@@ -34,7 +33,7 @@ class Room
   # end
 
   def to_s
-    "#{name}:\n\n#{desc}"
+    "#{name}:\n\n#{desc}\n\nExits: #{get_current_room_exits.to_s.delete("\"[]")}"
   end
 
   # TODO: print exits add upon this
